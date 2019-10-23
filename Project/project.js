4ad0820dbe722388fcac6026d8aed51bfcb973a7
Tatev Avalyan <tat.avalyan1999@gmail.com>
2019-10-23 17:54:07 +0400

    ball

--- Project/project.js
+++ Project/project.js
@@ -10,58 +10,82 @@ let left = true
 let top = true
 
 
-// document.addEventListener('mousedown',  function () {
-
-//   document.addEventListener('mousemove', function onMouseMove(event) {
-
-//     div.style.left = event.clientX - divWidth / 2 + 'px'
-//     div.style.top = event.clientY - divHeight / 2 + 'px'
-
-//     // document.addEventListener('mouseup')
-//   })
-// })
-
 div.addEventListener('mousedown', onMouseDown)
 
 function onMouseDown() {
   document.addEventListener('mousemove', onMouseMove)
   document.addEventListener('mouseup', onMouseUp)
+
 }
 
-function onMouseUp() {
+let pt2 = { x: 0, y: 0 }
+function onMouseUp(event) {
+  pt2 = {
+    x: event.clientX,
+    y: event.clientY
+  }
   document.removeEventListener('mousemove', onMouseMove)
+  console.log(pt2)
+  update()
+  return pt2
 }
 
 function onMouseMove() {
   div.style.left = event.clientX - divWidth / 2 + 'px'
   div.style.top = event.clientY - divHeight / 2 + 'px'
+
+}
+
+let pt1 = {
+  x: divleft,
+  y: divTop
+}
+
+function getAngle(pt1, pt2) {
+  return Math.atan((pt2.y - pt1.y) / (pt2.x - pt1.x)) * (180 / Math.PI)
 }
 
 
+let velX = 0
+let velY = 0
+function getVelocityFromAngle(angle) {
+  velX = Math.cos(angle)
+  velY = Math.sin(angle)
 
-function moveDiv() {
+  return { x: velX, y: velY }
+}
+//console.log(getAngle(pt1, pt2))
+
+console.warn(getVelocityFromAngle(Math.PI/6))
+
+
+function update() {
 
   if (divleft >= w - divWidth) left = false;
   if (divleft <= 0) left = true;
 
   if (left) {
-    divleft += 5
+    divleft += 15 * velX
     div.style.left = divleft + "px";
   } else {
-    divleft -= 5
+    divleft -= 15 * velX
     div.style.left = divleft + "px"
+
   }
 
   if (divTop >= h - divHeight) top = false;
   if (divTop <= 0) top = true;
 
   if (top) {
-    divTop += 5
+    divTop += 15 * velY
     div.style.top = divTop + 'px'
   } else {
-    divTop -= 5
+    divTop -= 15 * velY
     div.style.top = divTop + 'px'
   }
 
-  requestAnimationFrame(moveDiv)
+  requestAnimationFrame(update)
+
 }
+
+
